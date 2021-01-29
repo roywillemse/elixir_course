7e4b122ca932725d9b949afe1c1d9eb33d4204d4
Roy Willemse <roy.willemse@bettyblocks.com>
2021-01-29 10:01:53 +0100

    added socializer

--- finalProject/client/src/index.js
+++ finalProject/client/src/index.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  gql,
-} from '@apollo/client';
 import App from './App';
-
-const client = new ApolloClient({
-  uri: 'http://127.0.0.1:4000/graphql',
-  cache: new InMemoryCache(),
-});
-
-client
-  .query({
-    query: gql`
-      query {
-        allLinks {
-          id
-          description
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
